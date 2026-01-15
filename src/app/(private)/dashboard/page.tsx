28d829d14d27607729cbb3dcba34e62023b3cd0d
luizao <luizsantiago280@gmail.com>
2026-01-15 13:23:26 -0300

    :sparkles: feat: implement session management and user type definitions

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,3 +1,13 @@
-export default function Dashboard() {
+import auth from "@/lib/getSession";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+
+    const session = await auth();
+
+    if(!session) {
+        redirect("/");
+    }
+
     return (<h1>Dashboard Page</h1>);
 }
