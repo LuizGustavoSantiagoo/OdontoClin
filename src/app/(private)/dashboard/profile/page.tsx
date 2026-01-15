d126677355d74c008008286d1a78e5f284ca8040
luizao <luizsantiago280@gmail.com>
2026-01-15 14:20:51 -0300

    :sparkles: feat: enhance sidebar with profile link and update profile page to fetch user data

--- src/app/(private)/dashboard/profile/page.tsx
+++ src/app/(private)/dashboard/profile/page.tsx
@@ -1,10 +1,27 @@
-export default function Profile() {
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation";
+import { getUserData } from "./_data-acess/get-info-user";
+
+export default async function Profile() {
+
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const user = await getUserData({userId: session.user?.id})
+
+  if (!user) {
+    redirect("/");
+  }
+
   return (
     <section>
-        <div>
-          <h1>Dashboard</h1>
-          <p>Welcome to your dashboard!</p>
-        </div>
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome to your Profile</p>
+      </div>
     </section>
   );
 }
